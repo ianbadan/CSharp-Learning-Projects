0f8b4f79288a0a764c7a738759043ff7e6217182
Ian <ianbadan@live.com>
2021-02-09 19:47:56 -0300

    Commit contendo primeiro problema
    Primeiro problema resolvido com C#

---
+++ SistemaBanco/SistemaBanco/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace SistemaBanco
+{
+    class ContaBancaria
+    {
+        const int taxa = 5;
+        public int Codigo { get; private set; }
+        public string NomeTitular { get; set; }
+        public decimal Saldo { get; private set; }
+
+        public ContaBancaria(int codigo, string nomeTitular)
+        {
+            Codigo = codigo;
+            NomeTitular = nomeTitular;
+        }
+
+        public ContaBancaria(int codigo, string nomeTitular, decimal depositoInicial) : this(codigo, nomeTitular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(decimal valor)
+        {
+
+            Saldo = Saldo - valor - taxa;
+
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Codigo + ", Titular: " + NomeTitular + ", Saldo: " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ SistemaBanco/SistemaBanco/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SistemaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+            String nome;
+            decimal valor;
+            char hasDeposito;
+            ContaBancaria c1;
+
+
+            Console.Write("Informe o codigo da conta: ");
+            codigo = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do titular da conta: ");
+            nome = Console.ReadLine();
+            Console.Write("Haverá deposito inicial? (s/n): ");
+            hasDeposito = char.Parse(Console.ReadLine());
+            if (hasDeposito == 'S' || hasDeposito == 's')
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                valor = decimal.Parse(Console.ReadLine());
+                c1 = new ContaBancaria(codigo, nome, valor);
+            }
+            else
+            {
+                c1 = new ContaBancaria(codigo, nome);
+            }
+
+            Console.WriteLine("\nDados da conta \n" + c1);
+
+            Console.Write("Entre com o valor para depósito: ");
+            valor = decimal.Parse(Console.ReadLine());
+            c1.Deposito(valor);
+            Console.WriteLine("\nDados da conta atualizados:\n" + c1);
+
+            Console.Write("Entre com o valor para saque: ");
+            valor = decimal.Parse(Console.ReadLine());
+            c1.Saque(valor);
+            Console.WriteLine("\nDados da conta atualizados:\n" + c1);
+        }
+    }
+}
+
