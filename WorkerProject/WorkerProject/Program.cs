988a2bc0c9e9a3ff1f47033d854bc9c4c92323e1
Ian <ianbadan@live.com>
2021-02-12 15:33:15 -0300

    Remoção do projeto OrdemPedidos e adição do WorkerProject

---
+++ WorkerProject/WorkerProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using WorkerProject.Entities;
+using WorkerProject.Entities.Enums;
+
+namespace WorkerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many contracts to this worker: ");
+            int quantity = int.Parse(Console.ReadLine());
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine($"Enter #{i + 1} contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (Hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+            }
+
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+             string monthAndYear = Console.ReadLine();
+            double income = worker.Income(int.Parse((monthAndYear.Split('/'))[0]), int.Parse((monthAndYear.Split('/'))[1]));
+
+            Console.WriteLine("Worker: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ":  " + income.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
--- WorkerProject/WorkerProject/obj/Debug/netcoreapp3.1/WorkerProject.AssemblyInfo.cs
+++ WorkerProject/WorkerProject/obj/Debug/netcoreapp3.1/WorkerProject.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("OrderSystem")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WorkerProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("OrderSystem")]
-[assembly: System.Reflection.AssemblyTitleAttribute("OrderSystem")]
+[assembly: System.Reflection.AssemblyProductAttribute("WorkerProject")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WorkerProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
