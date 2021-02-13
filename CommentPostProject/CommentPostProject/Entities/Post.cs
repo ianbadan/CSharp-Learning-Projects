7ccc206d5755f520470bc8f06931677233e71cbd
Ian <ianbadan@live.com>
2021-02-13 02:27:48 -0300

    Adição de novo projeto Post Comments
    Simula um post de rede social com comentarios

---
+++ CommentPostProject/CommentPostProject/Entities/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommentPostProject.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; private set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            string text = Title + "\n" + Likes + " likes - " + Moment.ToString("dd/MM/yyyy HH:mm:ss") + "\n" + Content + "\n" + "Comments:\n";
+            foreach(Comment c in Comments)
+            {
+                text += c.Text + "\n";
+            }
+            return text;
+        }
+
+    }
+}
