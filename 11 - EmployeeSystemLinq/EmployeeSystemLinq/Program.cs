7e64f2d9e0ff89618fd61048d07e5814b9bca2b8
Ian <ianbadan@live.com>
2021-02-23 15:35:44 -0300

    Criação de novo Sistema Employee System Linq

---
+++ 11 - EmployeeSystemLinq/EmployeeSystemLinq/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using EmployeeSystemLinq.Entities;
+
+namespace EmployeeSystemLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(name, email, salary));
+                }
+
+                Console.Write("Enter Salary: ");
+                double sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                var emails = employees.Where(e => e.Salary > sal).OrderBy(e => e.Email).Select(e => e.Email);
+
+                Console.WriteLine("\nEmail of people whose salary is more than " + sal.ToString("F2", CultureInfo.InvariantCulture)+ ":");
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                var sum = employees.Where(e => e.Name[0] == 'M').Select(e => e.Salary).Sum();
+
+                Console.Write("\nSum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ 11 - EmployeeSystemLinq/README.md
@@ -0,0 +1,40 @@
+# Employee System Linq
+Sistema para demonstrar o uso de expressões Lambda, delegates e LINQ dentro da linguagem C#, mostrando a parte do paradigma funcional da linguagem.
+
+## Descrição
+
+Fazer um programa para ler os dados (nome, email e salário) de funcionários a partir de um arquivo em formato .txt. Em seguida mostrar, em ordem alfabética, o email dos funcionários cujo salário seja superior a um dado valor
+fornecido pelo usuário. 
+
+Mostrar também a soma dos salários dos funcionários cujonome começa com a letra 'M'.
+
+## Exemplo de arquivo 
+
+Maria,maria@gmail.com,3200.00
+
+Alex,alex@gmail.com,1900.00
+
+Marco,marco@gmail.com,1700.00
+
+Bob,bob@gmail.com,3500.00
+
+Anna,anna@gmail.com,2800.00
+
+
+# Exemplo de execução
+
+Enter full file path: **c:\temp\in.txt**
+
+Enter salary: **2000.00**
+
+
+Email of people whose salary is more than 2000.00:
+
+anna@gmail.com
+
+bob@gmail.com
+
+maria@gmail.com
+
+
+Sum of salary of people whose name starts with 'M': 4900.00
